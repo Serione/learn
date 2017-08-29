e7e55c5d51cf3fb447533910aad67d41bf8af21a
Xmd <1327789498@qq.com>
2017-08-29 08:59:05 +0000

    修改

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -3,7 +3,7 @@
   <div class="jumbotron">
     <h1>Hello Laravel</h1>
     <p class="lead">
-      你现在所看到的是 <a href="https://fsdhub.com/books/laravel-essential-training-5.1">Laravel APP</a> 的示例项目主页。
+      你现在所看到的是 <a href="https://fsdhub.com/books/laravel-essential-training-5.1">Learn APP</a> 的示例项目主页。
     </p>
     <p>
       一切，将从这里开始。
